429788935b9a8f9f74233980483eb06da21de88f
Ego_Code <criss.2004.guaman@gmail.com>
2026-04-23 11:07:17 -0500

    Implementacion de clase diagnostico

--- src/Diagnostico.java
+++ src/Diagnostico.java
@@ -1,2 +1,18 @@
 public class Diagnostico {
+    private String descripcion;
+
+    public Diagnostico(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public void clon(){
+        this.descripcion = this.descripcion;
+    }
 }
